58024c7966839ed774c101426cff78a152f0ee83
Mikel Sanabria <mikel.sanabria@gmail.com>
2021-06-09 01:08:09 +0200

    POST /customers end-point & tests #27

--- src/main/java/es/upm/miw/tfm/automundo/infrastructure/api/resources/CustomerResource.java
+++ src/main/java/es/upm/miw/tfm/automundo/infrastructure/api/resources/CustomerResource.java
@@ -1,12 +1,15 @@
 package es.upm.miw.tfm.automundo.infrastructure.api.resources;
 
 import es.upm.miw.tfm.automundo.domain.model.Customer;
+import es.upm.miw.tfm.automundo.domain.model.CustomerCreationUpdate;
 import es.upm.miw.tfm.automundo.domain.services.CustomerService;
 import es.upm.miw.tfm.automundo.infrastructure.api.dtos.CustomerLineDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping(CustomerResource.CUSTOMERS)
@@ -34,5 +37,11 @@ public class CustomerResource {
     @GetMapping(IDENTIFICATION_ID)
     public Mono<Customer> read(@PathVariable String identification) {
         return this.customerService.read(identification);
+    }
+
+    @PostMapping(produces = {"application/json"})
+    public Mono<Customer> create(@Valid @RequestBody CustomerCreationUpdate customerCreationUpdate) {
+        customerCreationUpdate.doDefault();
+        return this.customerService.create(customerCreationUpdate);
     }
 }
--- src/main/java/es/upm/miw/tfm/automundo/infrastructure/mongodb/persistence/CustomerPersistenceMongodb.java
+++ src/main/java/es/upm/miw/tfm/automundo/infrastructure/mongodb/persistence/CustomerPersistenceMongodb.java
@@ -1,7 +1,9 @@
 package es.upm.miw.tfm.automundo.infrastructure.mongodb.persistence;
 
+import es.upm.miw.tfm.automundo.domain.exceptions.ConflictException;
 import es.upm.miw.tfm.automundo.domain.exceptions.NotFoundException;
 import es.upm.miw.tfm.automundo.domain.model.Customer;
+import es.upm.miw.tfm.automundo.domain.model.CustomerCreationUpdate;
 import es.upm.miw.tfm.automundo.domain.persistence.CustomerPersistence;
 import es.upm.miw.tfm.automundo.infrastructure.mongodb.daos.CustomerReactive;
 import es.upm.miw.tfm.automundo.infrastructure.mongodb.entities.CustomerEntity;
@@ -31,5 +33,20 @@ public class CustomerPersistenceMongodb implements CustomerPersistence {
         return this.customerReactive.findByIdentificationId(identification)
                 .switchIfEmpty(Mono.error(new NotFoundException("Non existent customer with identification id: " + identification)))
                 .map(CustomerEntity::toCustomer);
+    }
+
+    @Override
+    public Mono<Customer> create(CustomerCreationUpdate customerCreationUpdate) {
+        return this.assertIdentificationIdNotExist(customerCreationUpdate.getIdentificationId())
+                .then(Mono.just(new CustomerEntity(customerCreationUpdate)))
+                .flatMap(this.customerReactive::save)
+                .map(CustomerEntity::toCustomer);
+    }
+
+    private Mono<Void> assertIdentificationIdNotExist(String identificationId) {
+        return this.customerReactive.findByIdentificationId(identificationId)
+                .flatMap(customerEntity -> Mono.error(
+                        new ConflictException("Customer identification id already exists : " + identificationId)
+                ));
     }
 }
