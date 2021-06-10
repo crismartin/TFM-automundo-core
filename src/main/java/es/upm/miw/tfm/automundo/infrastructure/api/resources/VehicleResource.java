749a7f783ee6a99c77897396d236680595ad5d7f
Cristian Fabian Martinez <cris.mtxz@gmail.com>
2021-06-10 21:05:57 +0200

    Changed End-Point to /vehicles/customer/{id}. #36

---
+++ src/main/java/es/upm/miw/tfm/automundo/infrastructure/api/resources/VehicleResource.java
@@ -0,0 +1,30 @@
+package es.upm.miw.tfm.automundo.infrastructure.api.resources;
+
+import es.upm.miw.tfm.automundo.domain.services.VehicleService;
+import es.upm.miw.tfm.automundo.infrastructure.api.dtos.VehicleLineDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+@RestController
+@RequestMapping(VehicleResource.VEHICLES)
+public class VehicleResource {
+    public static final String VEHICLES = "/vehicles";
+    public static final String CUSTOMERS_IDENTIFICATION = "/customer/{identification}";
+
+    private VehicleService vehicleService;
+
+    @Autowired
+    public VehicleResource(VehicleService vehicleService){
+        this.vehicleService = vehicleService;
+    }
+
+    @GetMapping(CUSTOMERS_IDENTIFICATION)
+    public Flux<VehicleLineDto> findVehiclesByIdCustomer(@PathVariable String identification) {
+        return this.vehicleService.findVehiclesByIdCustomer(identification)
+                .map(VehicleLineDto::new);
+    }
+}
