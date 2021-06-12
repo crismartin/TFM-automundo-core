acba2c6d0187dce82e100b2985bc8cfad53600fc
Mikel Sanabria <mikel.sanabria@gmail.com>
2021-06-12 21:04:07 +0200

    GET /technicians/{identification} end-point & tests #50

--- src/main/java/es/upm/miw/tfm/automundo/domain/persistence/TechnicianPersistence.java
+++ src/main/java/es/upm/miw/tfm/automundo/domain/persistence/TechnicianPersistence.java
@@ -3,10 +3,13 @@ package es.upm.miw.tfm.automundo.domain.persistence;
 import es.upm.miw.tfm.automundo.domain.model.Technician;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Repository
 public interface TechnicianPersistence {
 
     Flux<Technician> findByIdentificationIdAndNameAndSurNameAndActiveNullSafe(
             String identificationId, String name, String surName, Boolean active);
+
+    Mono<Technician> findByIdentificationId(String identification);
 }
--- src/main/java/es/upm/miw/tfm/automundo/infrastructure/api/resources/TechnicianResource.java
+++ src/main/java/es/upm/miw/tfm/automundo/infrastructure/api/resources/TechnicianResource.java
@@ -1,19 +1,19 @@
 package es.upm.miw.tfm.automundo.infrastructure.api.resources;
 
+import es.upm.miw.tfm.automundo.domain.model.Technician;
 import es.upm.miw.tfm.automundo.domain.services.TechnicianService;
 import es.upm.miw.tfm.automundo.infrastructure.api.dtos.TechnicianLineDto;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping(TechnicianResource.TECHNICIANS)
 public class TechnicianResource {
     public static final String TECHNICIANS = "/technicians";
     public static final String SEARCH = "/search";
+    public static final String IDENTIFICATION_ID = "/{identification}";
 
     private TechnicianService technicianService;
     @Autowired
@@ -28,6 +28,11 @@ public class TechnicianResource {
         return this.technicianService.findByIdentificationIdAndNameAndSurNameAndActiveNullSafe(
                 identificationId, name, surName, active)
                 .map(TechnicianLineDto::new);
+    }
+
+    @GetMapping(IDENTIFICATION_ID)
+    public Mono<Technician> read(@PathVariable String identification) {
+        return this.technicianService.read(identification);
     }
 
 }
